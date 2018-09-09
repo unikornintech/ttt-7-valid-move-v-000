cb5466a960bcdfe148080c647602068e3e775a9e
Lauren Days <lmchambers0420@gmail.com>
2018-09-09 23:17:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,10 +7,3 @@ def valid_move?(board, index)
         true
       end
   end
-   def position_taken?(board, index)
-    if board[index] == "X" || board[index] ==  "O"
-      true
-    elsif board[index] == " " || board[index] == "" || board[index] == nil
-      false
-    end
-  end
