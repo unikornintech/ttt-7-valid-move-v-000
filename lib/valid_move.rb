7d704d1f171d018938a4a686a4c5c2a136882ccd
Lauren Days <lmchambers0420@gmail.com>
2018-09-09 23:07:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,10 @@
 def valid_move?(board, index)
-  taken = true
+ def position_taken?(board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+ else
+     taken = true
+   end
+   taken
 end
