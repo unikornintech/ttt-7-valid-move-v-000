2f69cc46615e803c1990e7097da3ba62081bf571
Lauren Days <lmchambers0420@gmail.com>
2018-09-09 22:40:07 +0000

    automatically backed up by learn

---
+++ lib/valid_move.rb
@@ -0,0 +1,14 @@
+ttt_board = ["","X"," "," "," "," "," "," "," "]
+puts "Enter a position on the board 1-9:"
+user_input = gets.chomp
+number_entered = user_input.to_i - 1
+
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+end
+
+def position_taken?(board, index)
+ !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+valid_move?(number_entered, ttt_board)
